169cca72d70f70b6a483a6cbdcd29de913e31086
shivamkumarlnct <shivamsitu2@gmail.com>
2024-07-16 21:09:38 +0530

    n

--- maxsubarray.java
+++ maxsubarray.java
@@ -1,12 +1,15 @@
 public class maxsubarray {
     public static void main(String args[]){
-        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
+        int arr[]={5,4,-1,7,8};
         int max=Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];
-            if (sum >= max) {
+            if (sum > max) {
                 max=sum;
+            }
+            if(sum<0){ // it is defind the sum value is not negative
+                sum=0;
             }
         }
         System.out.println(sum);
