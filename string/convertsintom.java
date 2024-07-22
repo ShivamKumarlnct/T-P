a630962b276de09f5701f270d99434baab979817
shivamkumarlnct <shivamsitu2@gmail.com>
2024-07-22 20:38:14 +0530

    n

---
+++ string/convertsintom.java
@@ -0,0 +1,8 @@
+package string;
+
+public class convertsintom {//convert sentence into mobile number
+    public static void main(String args[]){
+        String str="GEEKSFORGEEKS";
+    }
+    
+}
