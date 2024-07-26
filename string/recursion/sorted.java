4652933a79527886390b00324f5e547376ee1b32
shivamkumarlnct <shivamsitu2@gmail.com>
2024-07-26 20:33:56 +0530

    n

--- string/recursion/sorted.java
+++ string/recursion/sorted.java
@@ -1,24 +1,21 @@
 package string.recursion;
 
 public class sorted {
-    public static boolean sorted(int arr[]){
-        int i=0;
-        if(i==arr.length){
+    public static boolean sorted(int arr[],int i){
+        if(i==arr.length-1){
             return true;
         }
 
         if(arr[i]>arr[i+1]){
-            sorted(i, i + 1);
             return false;
         }
-
-        return true;
+           return sorted(arr,i+1);
 
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        sorted(arr);
-        System.out.println(sorted(arr));
+        int arr[]={1,8,3,4,5};
+        sorted(arr,0);
+        System.out.println(sorted(arr,0));
     }
     
 }
